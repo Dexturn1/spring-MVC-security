9078bf26cb9201291f52441e9472295690103e15
Dexturn1 <kapkotiprabhat@gmail.com>
2025-09-04 16:41:21 +0530

    added the Custom Login form and Custom Error message

--- src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,13 +2,14 @@ package com.luv2code.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
-
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
@@ -31,5 +32,20 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+        )
+                .formLogin(form->form
+                        .loginPage("/showingLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll()
+                );
+        return http.build();
+    }
 }
+
+
