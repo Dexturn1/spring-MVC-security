d7fa57decd2aea227c425c2127e90aaf947f89e8
Dexturn1 <kapkotiprabhat@gmail.com>
2025-09-06 09:30:53 +0530

    Displaying Content based on Roles

--- src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
+++ src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.luv2code.springboot.demosecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
@@ -11,4 +12,9 @@ public class LoginController {
         return "fancy-login";
     }
 
+    //  Add request mapping for / access-denied
+    @RequestMapping("/access-denied")
+    public String showAccessDenied(){
+        return "access-denied";
+    }
 }
